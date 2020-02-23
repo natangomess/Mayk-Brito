df89f3cf856650cb25697f160f505b1485a71a86
Natan Gomes <55115100+natangomess@users.noreply.github.com>
2020-02-23 15:01:30 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,41 @@
+module.exports = [
+  {
+    id: "GykTLqODQuU",
+    title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
+    duration: "57min",
+    price: "Free",
+    featured: true
+  },
+  {
+    id: "vqrjFnq3-uo",
+    title: "Criando Player de Áudio com Javascript | Mayk Brito",
+    duration: "1h 45min",
+    price: "Free"
+  },
+  {
+    id: "2alg7MQ6_sI",
+    title: "Como Usar Git e Github na Prática: Guia Para Iniciantes | Mayk Brito",
+    duration: "33min",
+    price: "Free",
+    featured: true
+  },
+  {
+    id: "GSqR2i-Pq6o",
+    title: "Desvendando a Variável this no Javascript | Mayk Brito",
+    duration: "48min",
+    price: "Free"
+  },
+  {
+    id: "ghTrp1x_1As",
+    title: "O que é API REST e RESTful | Mayk Brito",
+    duration: "33 min",
+    price: "Free",
+    featured: true
+  },
+  {
+    id: "rAzHvYnQ8DY",
+    title: "Array: Higher Order Functions | Mayk Brito",
+    duration: "54min",
+    price: "Free"
+  }
+]
